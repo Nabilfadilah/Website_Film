37e9061c635c54bdc2b0d723ac372aeda6cae2a8
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-04 14:45:52 +0700

    Membuat data list film tranding

---
+++ movie/src/components/fragments/DataTrending.js
@@ -0,0 +1,74 @@
+// File data/cardData.js
+// kartu undangan, kartu nama, faktur, kwitansi, piagam, sertifikat, pamplet, brosur, yassin, banner, x banner, dan hangtag
+import film1 from "../../assets/film1.jpg";
+import film2 from "../../assets/film2.jpg";
+import film3 from "../../assets/film3.jpg";
+import film4 from "../../assets/film4.jpg";
+import film5 from "../../assets/film5.jpg";
+import film6 from "../../assets/film6.jpg";
+import film7 from "../../assets/film7.jpg";
+import film8 from "../../assets/film8.jpg";
+import film9 from "../../assets/film9.jpg";
+
+const cardsTranding = [
+    {
+        id: 4,
+        image: film1,
+        title: "Seni Memahami Kekasih",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 5,
+        image: film2,
+        title: "Hounds of War",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 6,
+        image: film3,
+        title: "Kaka Bos",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 7,
+        image: film4,
+        title: "Kang Mak",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 9,
+        image: film6,
+        title: "The Crow",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 10,
+        image: film7,
+        title: "Blink Twice",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 11,
+        image: film8,
+        title: "Trap",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 12,
+        image: film9,
+        title: "Alien Romulus",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+  ];
+  
+  export default cardsTranding;
+
+  
