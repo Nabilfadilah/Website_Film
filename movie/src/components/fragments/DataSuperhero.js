e2359a2fe2ffaa557e6e63235262ba03786b12ba
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-04 14:46:04 +0700

    Membuat data list film superhero

---
+++ movie/src/components/fragments/DataSuperhero.js
@@ -0,0 +1,41 @@
+// File data/cardData.js
+// kartu undangan, kartu nama, faktur, kwitansi, piagam, sertifikat, pamplet, brosur, yassin, banner, x banner, dan hangtag
+import movie1 from "../../assets/Batman.jpg";
+import movie2 from "../../assets/Flash.jpg";
+import movie3 from "../../assets/Naruto.jpg";
+import film5 from "../../assets/film5.jpg";
+
+const cardsSuperhero = [
+    {
+        id: 1,
+        image: movie1,
+        title: "Batman",
+        description: "Harga murah kualitas tidak murahan (bisa ambil contoh sendiri), terdapat berbagai desain undangan.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 2,
+        image: movie2,
+        title: "Flash",
+        description: "Desain elegan dengan pilihan bahan berkualitas tinggi untuk memperkuat citra profesional Anda.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 3,
+        image: movie3,
+        title: "Naruto",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+    {
+        id: 8,
+        image: film5,
+        title: "DeadPool",
+        description: "Cetak faktur dengan format yang jelas dan mudah dibaca, cocok untuk bisnis skala kecil hingga.",
+        info: "Last updated 3 mins ago",
+    },
+  ];
+  
+  export default cardsSuperhero;
+
+  
